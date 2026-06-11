3fb3584fe1a3ea6bb3eb82e6a9c4582a4473f823
dji220506 <dji220506@gmail.com>
2026-06-11 23:04:18 +0200

    Update results.js

--- results.js
+++ results.js
@@ -1,89 +1,204 @@
 const RESULTS = {
-  groups: {},
-  thirdPlace: [],
-  groupMatches: {},
-
-  knockout: {
-    round32: [],
-    round16: [],
-    quarterfinals: [],
-    semifinals: [],
-
-    champion: "",
-    runnerUp: "",
-    finalists: [],
-
-    thirdPlaceWinner: "",
-    final: "",
-    thirdPlace: "",
-
-    matches: {
-      round32: [
-        // {
-        //   match: 73,
-        //   team1: "",
-        //   team2: "",
-        //   winner: ""
-        // }
-      ],
-
-      round16: [
-        // {
-        //   match: 89,
-        //   team1: "",
-        //   team2: "",
-        //   winner: ""
-        // }
-      ],
-
-      quarterfinals: [
-        // {
-        //   match: 97,
-        //   team1: "",
-        //   team2: "",
-        //   winner: ""
-        // }
-      ],
-
-      semifinals: [
-        // {
-        //   match: 101,
-        //   team1: "",
-        //   team2: "",
-        //   winner: ""
-        // }
-      ],
-
-      thirdPlace: [
-        // {
-        //   match: 103,
-        //   team1: "",
-        //   team2: "",
-        //   winner: ""
-        // }
-      ],
-
-      final: [
-        // {
-        //   match: 104,
-        //   team1: "",
-        //   team2: "",
-        //   winner: ""
-        // }
-      ]
+  "groups": {
+    "A": [
+      "Mexico",
+      "South Africa",
+      "Czech Republic",
+      "South Korea"
+    ],
+    "B": [
+      "Switzerland",
+      "Canada",
+      "Qatar",
+      "Bosnia & Herzegovina"
+    ],
+    "C": [
+      "Brazil",
+      "Scotland",
+      "Morocco",
+      "Haiti"
+    ],
+    "D": [
+      "Turkey",
+      "Paraguay",
+      "USA",
+      "Australia"
+    ],
+    "E": [
+      "Ivory Coast",
+      "Curaçao",
+      "Ecuador",
+      "Germany"
+    ],
+    "F": [
+      "Netherlands",
+      "Japan",
+      "Sweden",
+      "Tunisia"
+    ],
+    "G": [
+      "Egypt",
+      "Belgium",
+      "New Zealand",
+      "Iran"
+    ],
+    "H": [
+      "Uruguay",
+      "Cape Verde",
+      "Spain",
+      "Saudi Arabia"
+    ],
+    "I": [
+      "Norway",
+      "France",
+      "Iraq",
+      "Senegal"
+    ],
+    "J": [
+      "Jordan",
+      "Argentina",
+      "Algeria",
+      "Austria"
+    ],
+    "K": [
+      "DR Congo",
+      "Portugal",
+      "Colombia",
+      "Uzbekistan"
+    ],
+    "L": [
+      "Ghana",
+      "Panama",
+      "Croatia",
+      "England"
+    ]
+  },
+  "thirdPlace": [],
+  "groupMatches": {
+    "A": {
+      "Mexico__South Africa": { "home": 2, "away": 0 },
+      "Mexico__South Korea": null,
+      "Mexico__Czech Republic": null,
+      "South Africa__South Korea": null,
+      "South Africa__Czech Republic": null,
+      "South Korea__Czech Republic": null
+    },
+    "B": {
+      "Canada__Switzerland": null,
+      "Canada__Qatar": null,
+      "Canada__Bosnia & Herzegovina": null,
+      "Switzerland__Qatar": null,
+      "Switzerland__Bosnia & Herzegovina": null,
+      "Qatar__Bosnia & Herzegovina": null
+    },
+    "C": {
+      "Brazil__Haiti": null,
+      "Brazil__Morocco": null,
+      "Brazil__Scotland": null,
+      "Haiti__Morocco": null,
+      "Haiti__Scotland": null,
+      "Morocco__Scotland": null
+    },
+    "D": {
+      "Turkey__Paraguay": null,
+      "Turkey__Australia": null,
+      "Turkey__USA": null,
+      "Paraguay__Australia": null,
+      "Paraguay__USA": null,
+      "Australia__USA": null
+    },
+    "E": {
+      "Curaçao__Ecuador": null,
+      "Curaçao__Germany": null,
+      "Curaçao__Ivory Coast": null,
+      "Ecuador__Germany": null,
+      "Ecuador__Ivory Coast": null,
+      "Germany__Ivory Coast": null
+    },
+    "F": {
+      "Japan__Netherlands": null,
+      "Japan__Tunisia": null,
+      "Japan__Sweden": null,
+      "Netherlands__Tunisia": null,
+      "Netherlands__Sweden": null,
+      "Tunisia__Sweden": null
+    },
+    "G": {
+      "Egypt__Belgium": null,
+      "Egypt__Iran": null,
+      "Egypt__New Zealand": null,
+      "Belgium__Iran": null,
+      "Belgium__New Zealand": null,
+      "Iran__New Zealand": null
+    },
+    "H": {
+      "Uruguay__Spain": null,
+      "Uruguay__Saudi Arabia": null,
+      "Uruguay__Cape Verde": null,
+      "Spain__Saudi Arabia": null,
+      "Spain__Cape Verde": null,
+      "Saudi Arabia__Cape Verde": null
+    },
+    "I": {
+      "France__Iraq": null,
+      "France__Senegal": null,
+      "France__Norway": null,
+      "Iraq__Senegal": null,
+      "Iraq__Norway": null,
+      "Senegal__Norway": null
+    },
+    "J": {
+      "Argentina__Jordan": null,
+      "Argentina__Algeria": null,
+      "Argentina__Austria": null,
+      "Jordan__Algeria": null,
+      "Jordan__Austria": null,
+      "Algeria__Austria": null
+    },
+    "K": {
+      "Portugal__Uzbekistan": null,
+      "Portugal__DR Congo": null,
+      "Portugal__Colombia": null,
+      "Uzbekistan__DR Congo": null,
+      "Uzbekistan__Colombia": null,
+      "DR Congo__Colombia": null
+    },
+    "L": {
+      "England__Ghana": null,
+      "England__Croatia": null,
+      "England__Panama": null,
+      "Ghana__Croatia": null,
+      "Ghana__Panama": null,
+      "Croatia__Panama": null
     }
   },
-
-  semifinalists: [],
-  finalists: [],
-
-  champion: "",
-  runnerUp: "",
-  thirdPlaceWinner: "",
-
-"awards": {
+  "knockout": {
+    "round32": [],
+    "round16": [],
+    "quarterfinals": [],
+    "semifinals": [],
+    "champion": null,
+    "runnerUp": null,
+    "finalists": [],
+    "thirdPlaceWinner": null,
+    "final": null,
+    "thirdPlace": null,
+    "matches": {
+      "round32": [],
+      "round16": [],
+      "quarterfinals": [],
+      "semifinals": [],
+      "thirdPlace": [],
+      "final": []
+    }
+  },
+  "semifinalists": [],
+  "finalists": [],
+  "champion": null,
+  "runnerUp": null,
+  "thirdPlaceWinner": null,
+  "awards": {
     "goldenBoot": [],
-
-    goldenBall: []
+    "goldenBall": []
   }
 };
