e1908c65b059986239fc917951d6abd80099ce6f
dji220506 <dji220506@gmail.com>
2026-06-12 12:05:44 +0200

    Update results.js

--- results.js
+++ results.js
@@ -1,7 +1,8 @@
 const RESULTS = {
   "groupMatches": {
     "A": {
-      "Mexico__South Africa": { "home": 2, "away": 0 }
+      "Mexico__South Africa": { "home": 2, "away": 0 },
+      "South Korea__Czech Republic": { "home": 2, "away": 1 }
     }
   }
 };
