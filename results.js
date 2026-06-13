40d7e074fac24bf6ad11d118aa524e53a89a9b24
dji220506 <dji220506@gmail.com>
2026-06-13 08:25:34 +0200

    Update results.js

--- results.js
+++ results.js
@@ -3,6 +3,13 @@ const RESULTS = {
     "A": {
       "Mexico__South Africa": { "home": 2, "away": 0 },
       "South Korea__Czech Republic": { "home": 2, "away": 1 }
+    },
+    "B": {
+      "Canada__Bosnia & Herzegovina": { "home": 1, "away": 1 }
+    },
+    "D": {
+      "Paraguay__USA": { "home": 1, "away": 4 }
     }
   }
 };
+
